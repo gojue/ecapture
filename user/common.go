126b03776b1db6490107538e9c607e1ed655314d
CFC4N <cfc4n.cs@gmail.com>
2022-06-19 00:11:03 +0800

    New feature: #87 support Android 12+ (openssl module)
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- user/common.go
+++ user/common.go
@@ -24,19 +24,10 @@ func inet_ntop(ip uint32) string {
 }
 
 func GetDynLibDirs() []string {
-	dirs, err := ParseDynLibConf("/etc/ld.so.conf")
+	dirs, err := ParseDynLibConf(LD_LOAD_PATH)
 	if err != nil {
 		log.Println(err.Error())
-		/*
-		   1, the RPATH binary header (set at build-time) of the library causing the lookup (if any)
-		   2, the RPATH binary header (set at build-time) of the executable
-		   3, the LD_LIBRARY_PATH environment variable (set at run-time)
-		   4, the RUNPATH binary header (set at build-time) of the executable
-		   5, /etc/ld.so.cache
-		   6, base library directories (/lib and /usr/lib)
-		   ref: http://blog.tremily.us/posts/rpath/
-		*/
-		return []string{"/lib", "/usr/lib", "/usr/lib64", "/lib64"}
+		return default_so_paths
 	}
 	return append(dirs, "/lib64", "/usr/lib64")
 }
---
+++ user/common_android12.go
@@ -0,0 +1,17 @@
+//go:build android12
+// +build android12
+
+package user
+
+const (
+	LD_LOAD_PATH = "/etc/ld.so.conf"
+)
+
+// https://source.android.com/devices/architecture/vndk/linker-namespace
+var (
+	default_so_paths = []string{
+		"/data/asan/system/lib64",
+		"/apex/com.android.conscrypt/lib64",
+		"/apex/com.android.runtime/lib64/bionic",
+	}
+)
