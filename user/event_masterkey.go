bb99e963288e34866a33489dc4682c019d33e1b1
CFC4N <cfc4n.cs@gmail.com>
2022-07-22 23:48:09 +0800

    user : add TLS 1.3 master secret event.
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- user/event_masterkey.go
+++ user/event_masterkey.go
@@ -12,21 +12,28 @@ import (
 const (
 	SSL3_RANDOM_SIZE      = 32
 	MASTER_SECRET_MAX_LEN = 48
+	EVP_MAX_MD_SIZE       = 64
 )
 
 /*
 	u8 client_random[SSL3_RANDOM_SIZE];
     u8 master_key[MASTER_SECRET_MAX_LEN];
 */
-type MasterKeyEvent struct {
-	module       IModule
-	event_type   event_processor.EventType
-	ClientRandom [SSL3_RANDOM_SIZE]byte
-	MasterKey    [MASTER_SECRET_MAX_LEN]byte
-	payload      string
+type MasterSecretEvent struct {
+	module                       IModule
+	event_type                   event_processor.EventType
+	Version                      int32 // TLS version
+	ClientRandom                 [SSL3_RANDOM_SIZE]byte
+	MasterKey                    [MASTER_SECRET_MAX_LEN]byte
+	ServerHandshakeTrafficSecret [EVP_MAX_MD_SIZE]byte
+	ExporterSecret               [EVP_MAX_MD_SIZE]byte
+	ServerTrafficSecret0         [EVP_MAX_MD_SIZE]byte
+	ClientHandshakeTrafficSecret [EVP_MAX_MD_SIZE]byte
+	ClientTrafficSecret0         [EVP_MAX_MD_SIZE]byte
+	payload                      string
 }
 
-func (this *MasterKeyEvent) Decode(payload []byte) (err error) {
+func (this *MasterSecretEvent) Decode(payload []byte) (err error) {
 	buf := bytes.NewBuffer(payload)
 	if err = binary.Read(buf, binary.LittleEndian, &this.ClientRandom); err != nil {
 		return
@@ -38,43 +45,43 @@ func (this *MasterKeyEvent) Decode(payload []byte) (err error) {
 	return nil
 }
 
-func (this *MasterKeyEvent) StringHex() string {
+func (this *MasterSecretEvent) StringHex() string {
 	s := fmt.Sprintf("ClientRandom:%02x, MasterKey:%02x", this.ClientRandom, this.MasterKey)
 	return s
 }
 
-func (this *MasterKeyEvent) String() string {
+func (this *MasterSecretEvent) String() string {
 	s := fmt.Sprintf("ClientRandom:%02x, MasterKey:%02x", this.ClientRandom, this.MasterKey)
 	return s
 }
 
-func (this *MasterKeyEvent) SetModule(module IModule) {
+func (this *MasterSecretEvent) SetModule(module IModule) {
 	this.module = module
 }
 
-func (this *MasterKeyEvent) Module() IModule {
+func (this *MasterSecretEvent) Module() IModule {
 	return this.module
 }
 
-func (this *MasterKeyEvent) Clone() event_processor.IEventStruct {
-	event := new(MasterKeyEvent)
+func (this *MasterSecretEvent) Clone() event_processor.IEventStruct {
+	event := new(MasterSecretEvent)
 	event.module = this.module
 	event.event_type = event_processor.EventTypeModuleData
 	return event
 }
 
-func (this *MasterKeyEvent) EventType() event_processor.EventType {
+func (this *MasterSecretEvent) EventType() event_processor.EventType {
 	return this.event_type
 }
 
-func (this *MasterKeyEvent) GetUUID() string {
+func (this *MasterSecretEvent) GetUUID() string {
 	return fmt.Sprintf("%02X", this.ClientRandom)
 }
 
-func (this *MasterKeyEvent) Payload() []byte {
+func (this *MasterSecretEvent) Payload() []byte {
 	return []byte(this.payload)
 }
 
-func (this *MasterKeyEvent) PayloadLen() int {
+func (this *MasterSecretEvent) PayloadLen() int {
 	return len(this.payload)
 }
