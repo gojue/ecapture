c0f43d8e5836b4b41ac2db6ac8320955d2bf56a8
CFC4N <cfc4n.cs@gmail.com>
2022-08-06 11:46:59 +0800

    kern : support BTF disabled.
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- kern/ecapture.h
+++ kern/ecapture.h
@@ -10,10 +10,12 @@
 #include "bpf/bpf_endian.h"
 
 #else
+//CO:RE is disabled
 #include <linux/kconfig.h>
 #include <uapi/linux/ptrace.h>
 #include <linux/bpf.h>
 #include <linux/socket.h>
+#include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 #include <bpf/bpf_endian.h>
@@ -21,6 +23,11 @@
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/in.h>
+
+struct tcphdr {
+    __be16 source;
+    __be16 dest;
+};
 
 #endif
 
