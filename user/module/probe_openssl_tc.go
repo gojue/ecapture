d36fe99a1fe1978bc828e8f5fc2fc958968fea42
CFC4N <cfc4n.cs@gmail.com>
2022-11-05 17:18:05 +0800

    docs : change License to Apache License 2.0
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- user/module/probe_openssl_tc.go
+++ user/module/probe_openssl_tc.go
@@ -1,3 +1,17 @@
+// Copyright 2022 CFC4N <cfc4n.cs@gmail.com>. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package module
 
 import (
@@ -226,7 +240,7 @@ func (this *MOpenSSLProbe) createPcapng(netIfs []net.Interface) error {
 			Hardware:    "eCapture Hardware",
 			OS:          "",
 			Application: "ecapture.lua",
-			Comment:     "see https://ecapture.cc for more information. CFC4N <cfc4n@cnxct.com>",
+			Comment:     "see https://ecapture.cc for more information. CFC4N <cfc4n.cs@gmail.com>",
 		},
 	}
 	// write interface description
