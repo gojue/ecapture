363011120e0781cd06c7b061a2bbe4b3abdd507a
CFC4N <cfc4n.cs@gmail.com>
2023-03-15 22:53:56 +0800

    user: add ifname's default value of gotls module.
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- user/config/config_gotls.go
+++ user/config/config_gotls.go
@@ -1,3 +1,16 @@
+// Copyright 2022 CFC4N <cfc4n.cs@gmail.com>. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 // Copyright © 2022 Hengqi Chen
 package config
 
@@ -28,6 +41,11 @@ func (c *GoTLSConfig) Check() error {
 	if c.Path == "" {
 		return ErrorGoBINNotSET
 	}
+
+	if c.Ifname == "" || len(c.Ifname) == 0 {
+		c.Ifname = DefaultIfname
+	}
+
 	_, err := os.Stat(c.Path)
 	return err
 }
