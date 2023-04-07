1c6afae21eb14b3e8b6f291add4e4e6291426eb3
CFC4N <cfc4n.cs@gmail.com>
2023-04-07 23:53:03 +0800

    pkg : build a new ELF binary when unit testing worked.
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- pkg/proc/proc_test.go
+++ pkg/proc/proc_test.go
@@ -1,13 +1,15 @@
 package proc
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 )
 
-const ELF_BUILD_BY_CGO = "go_elf/go_elf"
+const ELF_BUILD_BY_CGO = "go_elf"
 
 func TestExtraceGoVersion(t *testing.T) {
 	path := fmt.Sprintf("/proc/%d/exe", os.Getppid())
@@ -22,17 +24,50 @@ func TestExtraceGoVersion(t *testing.T) {
 // cd go_elf
 // CGO_ENABLED=1 go build .
 func TestExtraceGoVersionGccgo(t *testing.T) {
+	e := os.Chdir("go_elf")
+	if e != nil {
+		t.Fatalf("chdir error:%v\n", e)
+	}
+
 	p, e := os.Getwd()
 	if e != nil {
 		t.Fatalf("Getwd error:%v", e)
 	}
 	t.Logf("pwd:%s", p)
 
+	// go build go_elf
+	pathEnv := os.Getenv("PATH")
+	t.Logf("env $PATH:%s", pathEnv)
+
+	// mkdir directories
+	goBuildPath := filepath.Join(os.TempDir(), "go-build")
+	goEnvPath := filepath.Join(os.TempDir(), "go-env")
+	e = os.MkdirAll(goBuildPath, os.ModePerm)
+	if e != nil {
+		t.Fatal(e)
+	}
+	e = os.MkdirAll(goEnvPath, os.ModePerm)
+	if e != nil {
+		t.Fatal(e)
+	}
+
+	c := exec.Command("go", "build", "-v", ".")
+
+	var outb, errb bytes.Buffer
+	c.Stdout = &outb
+	c.Stderr = &errb
+	e = c.Run()
+	t.Logf("output:%s, errput:%s", outb.String(), errb.String())
+	if e != nil {
+		c.Stderr = os.Stderr
+		t.Fatalf("go build failed:%v", e)
+	}
+
 	p1 := filepath.Join(p, ELF_BUILD_BY_CGO)
 	ver, err := ExtraceGoVersion(p1)
+	t.Logf("Extrace GoVersion from CGO ELF :%s", p1)
 	if err != nil {
-		t.Log(err)
-		return
+		t.Fatal(err)
 	}
-	t.Logf("Extrace GoVersion %v from CGO ELF :%s", ver, ELF_BUILD_BY_CGO)
+	t.Logf("version found :%v", ver)
 }
