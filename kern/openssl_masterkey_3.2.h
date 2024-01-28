07301c845c24073c13c2bdbe0bbf7fd1c86c0263
CFC4N <cfc4n.cs@gmail.com>
2024-01-28 21:38:08 +0800

    kern: fix non-void function 'probe_ssl_master_key' should return a value.
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- kern/openssl_masterkey_3.2.h
+++ kern/openssl_masterkey_3.2.h
@@ -114,7 +114,7 @@ int probe_ssl_master_key(struct pt_regs *ctx) {
         // 重新获取 ssl->tls 的地址作为 SSL_CONNECTION的地址
         // Reget the address of the ssl->tls as the address of the SSL_CONNECTION
         debug_bpf_printk("unsupported type: SSL_CONNECTION, coming soon.\n");
-        return;
+        return 0;
     }
 
     // Get SSL->version pointer
