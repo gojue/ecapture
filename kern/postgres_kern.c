71b44e25843fcb5e2d146282af76850a2cc38095
CFC4N <cfc4n.cs@gmail.com>
2024-08-25 12:07:36 +0800

    kern: support uid/pid filter in ebpf TC hook.
    remove uid/pid filter in kprobe/tcp_sendmsg.
    
    tcp_sendmsg hook all processes are monitored, so there is no need to filter pid and uid, otherwise pid\uid cannot be used in the TC capture_packets function to filter network packets
    
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- kern/postgres_kern.c
+++ kern/postgres_kern.c
@@ -36,10 +36,14 @@ SEC("uprobe/exec_simple_query")
 int postgres_query(struct pt_regs *ctx) {
     u64 current_pid_tgid = bpf_get_current_pid_tgid();
     u32 pid = current_pid_tgid >> 32;
-
+    u64 current_uid_gid = bpf_get_current_uid_gid();
+    u32 uid = current_uid_gid;
 #ifndef KERNEL_LESS_5_2
     // if target_ppid is 0 then we target all pids
     if (target_pid != 0 && target_pid != pid) {
+        return 0;
+    }
+    if (target_uid != 0 && target_uid != uid) {
         return 0;
     }
 #endif
