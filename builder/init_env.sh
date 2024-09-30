18789f128faa1ce234ab2cc0d63f6518ff734d4c
CFC4N <cfc4n.cs@gmail.com>
2024-09-30 21:20:36 +0800

    workflows: update linux source tgz file version.
    error output:
    Reading state information...
    E: Unable to locate package linux-source-6.8.0
    E: Couldn't find any package by glob 'linux-source-6.8.0'
    E: Couldn't find any package by regex 'linux-source-6.8.0'
    Error: Process completed with exit code 100.
    
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- builder/init_env.sh
+++ builder/init_env.sh
@@ -9,27 +9,28 @@ if [ $? -ne 0 ]; then
   exit
 fi
 
-CLANG_NUM=12
+CLANG_NUM=-12
 # shellcheck disable=SC2209
 MAKE_ECAPTURE=make
 if [ ${release_num} == "20.04" ]; then
-  CLANG_NUM=9
+  CLANG_NUM=-9
   MAKE_ECAPTURE="make nocore"
   elif [ ${release_num} == "20.10" ]; then
-  CLANG_NUM=10
+  CLANG_NUM=-10
   MAKE_ECAPTURE="make nocore"
   elif [ ${release_num} == "21.04" ]; then
-  CLANG_NUM=11
+  CLANG_NUM=-11
   elif [ ${release_num} == "21.10" ]; then
-  CLANG_NUM=12
+  CLANG_NUM=-12
   elif [ ${release_num} == "22.04" ]; then
-  CLANG_NUM=12
+  CLANG_NUM=-12
   elif [ ${release_num} == "22.10" ]; then
-  CLANG_NUM=12
+  CLANG_NUM=-12
   elif [ ${release_num} == "23.04" ];then
-  CLANG_NUM=15
+  CLANG_NUM=-15
   else
-    echo "unsupported release version ${release_num}" && exit
+    echo "used default CLANG Version"
+    CLANG_NUM=
 fi
 
 echo "CLANG_NUM=${CLANG_NUM}"
@@ -52,22 +53,21 @@ cd ~
 
 uname -a
 sudo apt-get update
-kernel_ver=`uname -r | cut -d'-' -f 1`
 # 环境安装
-sudo apt-get install --yes build-essential pkgconf libelf-dev llvm-${CLANG_NUM} clang-${CLANG_NUM} linux-tools-common linux-tools-generic gcc-aarch64-linux-gnu libssl-dev flex bison linux-source-${kernel_ver}
+sudo apt-get install --yes build-essential pkgconf libelf-dev llvm${CLANG_NUM} clang${CLANG_NUM} linux-tools-common linux-tools-generic gcc-aarch64-linux-gnu libssl-dev flex bison linux-source
 for tool in "clang" "llc" "llvm-strip"
 do
   sudo rm -f /usr/bin/$tool
-  sudo ln -s /usr/bin/$tool-${CLANG_NUM} /usr/bin/$tool
+  sudo ln -s /usr/bin/$tool${CLANG_NUM} /usr/bin/$tool
 done
 
 cd /usr/src
-sudo tar -xf linux-source-${kernel_ver}.tar.bz2
-cd /usr/src/linux-source-${kernel_ver}
+sudo tar -xf linux-source.tar.bz2
+cd /usr/src/linux-source
 test -f .config || yes "" | sudo make oldconfig
 yes "" | sudo make ARCH=${ARCH} CROSS_COMPILE=aarch64-linux-gnu- prepare V=0 > /dev/null
 yes "" | sudo make prepare V=0 > /dev/null
-ls -al /usr/src/linux-source-${kernel_ver}
+ls -al /usr/src/linux-source
 
 clang --version
 cd ~
