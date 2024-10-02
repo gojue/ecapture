83453976acb2c8347fc06253c8dfe859ec9f4795
CFC4N <cfc4n.cs@gmail.com>
2024-10-02 10:53:12 +0800

    makefile: fix Recursive variable 'KERN_HEADERS' references itself (eventually).
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- variables.mk
+++ variables.mk
@@ -108,9 +108,6 @@ HOST_VERSION_SHORT := $(shell uname -r | cut -d'-' -f 1)
 
 # linux-source-5.15.0.tar.bz2
 LINUX_SOURCE_PATH ?= /usr/src/linux-source-$(HOST_VERSION_SHORT)
-ifdef KERN_HEADERS
-	LINUX_SOURCE_PATH = $(KERN_HEADERS)
-endif
 
 ifdef CROSS_ARCH
 	ifeq ($(HOST_ARCH),aarch64)
@@ -174,8 +171,13 @@ endif
 #
 ifdef CROSS_ARCH
 	KERNEL_HEADER_GEN = test -e arch/$(LINUX_ARCH)/kernel/asm-offsets.s || yes "" | $(SUDO) make ARCH=$(LINUX_ARCH) CROSS_COMPILE=$(CMD_CC_PREFIX) prepare V=0
-	KERN_HEADERS = $(LINUX_SOURCE_PATH)
+	ifdef KERN_HEADERS
+		LINUX_SOURCE_PATH = $(KERN_HEADERS)
+	else
+		KERN_HEADERS = $(LINUX_SOURCE_PATH)
+    endif
 endif
+
 KERN_RELEASE ?= $(UNAME_R)
 KERN_BUILD_PATH ?= $(if $(KERN_HEADERS),$(KERN_HEADERS),/lib/modules/$(KERN_RELEASE)/build)
 KERN_SRC_PATH ?= $(if $(KERN_HEADERS),$(KERN_HEADERS),$(if $(wildcard /lib/modules/$(KERN_RELEASE)/source),/lib/modules/$(KERN_RELEASE)/source,$(KERN_BUILD_PATH)))
