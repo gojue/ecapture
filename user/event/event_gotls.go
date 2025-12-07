7b3d3a4dd149435e124524e4fa3541b3af908fd1
Hugo <77865234+zenyanle@users.noreply.github.com>
2025-12-07 16:20:48 +0800

    fix(gotls): correct event output logic and support protobuf (#891)
    * fix(gotls): correct event output logic and support protobuf
    
    Fix Dispatcher to properly handle GoTLS events for both text and Protobuf outputs.
    Remove redundant JSON encoding fields from event_base.
    
    -----
    
    Co-authored-by: cfc4n 709947+cfc4n@users.noreply.github.com

--- user/event/event_gotls.go
+++ user/event/event_gotls.go
@@ -3,7 +3,6 @@ package event
 
 import (
 	"bytes"
-	"encoding/base64"
 	"encoding/binary"
 	"fmt"
 
@@ -48,12 +47,7 @@ func (ge *GoTLSEvent) Decode(payload []byte) error {
 
 func (ge *GoTLSEvent) String() string {
 	s := fmt.Sprintf("PID: %d, Comm: %s, TID: %d, PayloadType:%d, Payload: %s\n", ge.Pid, string(ge.Comm[:]), ge.Tid, ge.inner.PayloadType, string(ge.Data[:ge.Len]))
-	eb := ge.Base()
-	p, e := eb.Encode()
-	if e != nil {
-		return s
-	}
-	return string(p)
+	return s
 }
 
 func (ge *GoTLSEvent) StringHex() string {
@@ -66,17 +60,16 @@ func (ge *GoTLSEvent) StringHex() string {
 
 func (ge *GoTLSEvent) Base() Base {
 	return Base{
-		Timestamp:     int64(ge.TimestampNS),
-		UUID:          ge.GetUUID(),
-		SrcIP:         "127.0.0.1", // GoTLS events do not have SrcIP
-		SrcPort:       0,           // GoTLS events do not have SrcPort
-		DstIP:         "127.0.0.1", // GoTLS events do not have DstIP
-		DstPort:       0,           // GoTLS events do not have DstPort
-		PID:           int64(ge.Pid),
-		PName:         string(ge.Comm[:]),
-		Type:          uint32(ge.inner.PayloadType),
-		Length:        uint32(ge.Len),
-		PayloadBase64: base64.StdEncoding.EncodeToString(ge.Data[:ge.Len]),
+		Timestamp: int64(ge.TimestampNS),
+		UUID:      ge.GetUUID(),
+		SrcIP:     "127.0.0.1", // GoTLS events do not have SrcIP
+		SrcPort:   0,           // GoTLS events do not have SrcPort
+		DstIP:     "127.0.0.1", // GoTLS events do not have DstIP
+		DstPort:   0,           // GoTLS events do not have DstPort
+		PID:       int64(ge.Pid),
+		PName:     commStr(ge.Comm[:]),
+		Type:      uint32(ge.inner.PayloadType),
+		Length:    uint32(ge.Len),
 	}
 }
 
