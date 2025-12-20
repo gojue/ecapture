e1c9f3706d44a2905419b235ca944489f4339120
CFC4N <cfc4n.cs@gmail.com>
2025-12-20 14:51:17 +0800

    refactor: streamline error handling and improve readability in OpenSSL probing functions
    Signed-off-by: CFC4N <cfc4n.cs@gmail.com>

--- kern/openssl.h
+++ kern/openssl.h
@@ -398,39 +398,39 @@ int probe_entry_SSL_read(struct pt_regs* ctx) {
         debug_bpf_printk(
             "(OPENSSL) bpf_probe_read ssl_ver_ptr failed, ret: %d\n",
             ret);
-    } else {
-    //  In BoringSSL, the ssl_st->s3->hs object is mainly used; ssl_st->version may be null
-        ssl_rbio_ptr = (u64 *)(ssl + SSL_ST_RBIO);
-            ret = bpf_probe_read_user(&ssl_rbio_addr, sizeof(ssl_rbio_addr),
-                                      ssl_rbio_ptr);
-            if (ret) {
-                debug_bpf_printk(
-                    "(OPENSSL) bpf_probe_read ssl_rbio_ptr failed, ret: %d\n",
-                    ret);
-            } else {
-                // get ssl->bio->method->type
-                bio_type = process_BIO_type(ssl_rbio_addr);
+    }
 
-                // get fd ssl->rbio->num
-                ssl_rbio_num_ptr = (u64 *)(ssl_rbio_addr + BIO_ST_NUM);
-                ret = bpf_probe_read_user(&ssl_rbio_num_addr, sizeof(ssl_rbio_num_addr),
-                                          ssl_rbio_num_ptr);
-                if (ret) {
-                    debug_bpf_printk(
-                        "(OPENSSL) bpf_probe_read ssl_rbio_num_ptr failed, ret: %d\n",
-                        ret);
-                } else {
-                    fd = (u32)ssl_rbio_num_addr;
-                    if (fd == 0) {
-                        u64 ssl_addr = (u64)ssl;
-                        u64 *fd_ptr = bpf_map_lookup_elem(&ssl_st_fd, &ssl_addr);
-                        if (fd_ptr) {
-                            fd = (u64)*fd_ptr;
-                        }
-                    }
-                    debug_bpf_printk("openssl uprobe/SSL_read fd: %d, version: %d\n", fd, ssl_version);
+    //  In BoringSSL, the ssl_st->s3->hs object is mainly used; ssl_st->version may be null
+    ssl_rbio_ptr = (u64 *)(ssl + SSL_ST_RBIO);
+    ret = bpf_probe_read_user(&ssl_rbio_addr, sizeof(ssl_rbio_addr),
+                              ssl_rbio_ptr);
+    if (ret) {
+        debug_bpf_printk(
+            "(OPENSSL) bpf_probe_read ssl_rbio_ptr failed, ret: %d\n",
+            ret);
+    } else {
+        // get ssl->bio->method->type
+        bio_type = process_BIO_type(ssl_rbio_addr);
+
+        // get fd ssl->rbio->num
+        ssl_rbio_num_ptr = (u64 *)(ssl_rbio_addr + BIO_ST_NUM);
+        ret = bpf_probe_read_user(&ssl_rbio_num_addr, sizeof(ssl_rbio_num_addr),
+                                  ssl_rbio_num_ptr);
+        if (ret) {
+            debug_bpf_printk(
+                "(OPENSSL) bpf_probe_read ssl_rbio_num_ptr failed, ret: %d\n",
+                ret);
+        } else {
+            fd = (u32)ssl_rbio_num_addr;
+            if (fd == 0) {
+                u64 ssl_addr = (u64)ssl;
+                u64 *fd_ptr = bpf_map_lookup_elem(&ssl_st_fd, &ssl_addr);
+                if (fd_ptr) {
+                    fd = (u64)*fd_ptr;
                 }
             }
+            debug_bpf_printk("openssl uprobe/SSL_read fd: %d, version: %d\n", fd, ssl_version);
+        }
     }
 
     const char* buf = (const char*)PT_REGS_PARM2(ctx);
